4dd29343df48b230b90fbcce534d19298673aff9
yql <yuanql@foxmail.com>
2023-03-21 23:41:11 +0800

    自己实现 react18 中的 useSyncExternalStore

--- react-redux/hooks/useSelector.jsx
+++ react-redux/hooks/useSelector.jsx
@@ -1,4 +1,4 @@
-import React, { useSyncExternalStore } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import ReactReduxContext from "../ReactReduxContext"
 
 
@@ -6,12 +6,30 @@ import ReactReduxContext from "../ReactReduxContext"
 function useSelector(selector) {
     const { store } = React.useContext(ReactReduxContext)
 
-    const selectedField = useSyncExternalStore(
+    // react18新增的自定义hooks，2个参数，参数1是外部仓库订阅的方法，参数2是获取快照的方法，获取最新的状态
+    // 使用同步的外部仓库
+    return useSyncExternalStore(
         store.subscribe,
         () => selector(store.getState())
     )
 
-    return selectedField
+}
+
+// 自己实现 useSyncExternalStore
+function useSyncExternalStore(subscribe, getSnapState) {
+    // 定义个状态，初始状态是快照的结果
+    let [state, setState] = useState(getSnapState())
+
+    // 内部也是个订阅
+    // 用 useLayoutEffect 订阅一次就可以了，不需要每次都订阅
+    // 如果写在外面，每次刷新组件都要订阅，不合理浪费性能
+    useLayoutEffect(() => {
+        // 当仓库状态发生改变，重新进行setState，把新快照传给state
+        subscribe(() => {
+            setState(getSnapState())
+        })
+    }, [])
+    return state
 }
 
 export default useSelector
