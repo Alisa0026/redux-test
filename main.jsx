292df20db8da9e65291ff2c090efecac13b727a3
yql <yuanql@foxmail.com>
2023-03-22 12:02:12 +0800

    修改react18中使用 createRoot，不是render

--- main.jsx
+++ main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from "./react-redux";
 import Counter1 from './components/Counter1'
 import Counter2 from './components/Counter2'
 // 使用 react-redux
 import store from "./store"
 
+
 // 最外层引入store，然后通过Provider向下层所有组件传递，那么下面所有组件都可以拿到store
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <Counter1 />
         <hr />
         <Counter2 />
     </Provider>
-    , document.getElementById('root'))
+)
