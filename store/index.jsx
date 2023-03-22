eb0e9cbb31cf4b9c3fed343734ddd3c4d03672cd
yql <yuanql@foxmail.com>
2023-03-22 10:54:19 +0800

    实现异步操作简单原理，但是侵入性太强不好，需要改进

--- store/index.jsx
+++ store/index.jsx
@@ -5,4 +5,24 @@ import combineReducer from "./reducers";
 
 const store = createStore(combineReducer)
 
+// 实现异步操作改造 dispatch
+// 获取老的方法
+const oldDispatch = store.dispatch
+/*
+store.dispatch = function (action) {
+    // 在新的方法里，调用老的方法延迟1s去派发action
+    setTimeout(() => {
+        oldDispatch(action)
+    }, 1000);
+    return action
+}
+*/
+
+// 实现打印日志
+// 状态变更前打印老状态，变更后打印新状态
+store.dispatch = function (action) {
+    console.log('prev state', store.getState());
+    oldDispatch(action)
+    console.log('next state', store.getState());
+}
 export default store
